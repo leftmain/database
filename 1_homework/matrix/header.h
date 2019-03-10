4dfa4cbe181fb04f63fe4ceef7a7aedbe1312b50
leftmain <leftmain@yandex.ru>
2019-03-11 02:03:13 +0300

    rm trash

--- 1_homework/matrix/header.h
+++ 1_homework/matrix/header.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 // io
 int read_matrix(double *, int, const char *);
--- 1_homework/matrix/main.cpp
+++ 1_homework/matrix/main.cpp
@@ -1,5 +1,6 @@
 #include "header.h"
 
+int check(double *, double *, int, const char *);
 double f(int, int);
 double f(int i, int j) {
 	return (double)(abs(2*i + j) % 10);
@@ -8,7 +9,7 @@ double f(int i, int j) {
 int main(int argc, char ** argv) {
 	double * a = 0;
 	double * b = 0;
-	int n = 8192;
+	int n = 13;
 	int res = 0;
 	double t1 = 0;
 	double t2 = 0;
@@ -20,9 +21,8 @@ int main(int argc, char ** argv) {
 		return -1;
 	}
 	if (argc == 3) file = argv[2];
-	if (n == 1) return 1;
-	if (n & 1) n--;
-printf("# %d\n", n);
+	n = pow(2, n);
+	printf("n = %d\n", n);
 
 	a = (double *)malloc(n * n * sizeof(double));
 	if (!a) {
@@ -61,6 +61,11 @@ printf("# %d\n", n);
 	t1 = (clock() - t1) / CLOCKS_PER_SEC;
 	printf("Bad time: %.2lf\n", t1);
 	print_matrix(a, n);
+	if (check(a, b, n, file)) {
+		free(a);
+		free(b);
+		return 0;
+	}
 
 	printf("Now cache-blocked algorithm\n");
 
@@ -69,46 +74,10 @@ printf("# %d\n", n);
 	t2 = (clock() - t2) / CLOCKS_PER_SEC;
 	print_matrix(a, n);
 	printf("Block time: %.2lf\n", t2);
-	if (file) {
-		printf("Checking now\nReading original file..\n");
-		if ((res = read_matrix(b, n, file)) < 0) {
-			switch(res) {
-				case -1:
-					printf("Cannot open %s\n", file);
-					break;
-				case -2:
-					printf("Cannot read %s\n", file);
-					break;
-				default:
-					printf("Unknown error\n");
-			}
-			free(a);
-			free(b);
-			return -3;
-		}
-		printf("Comparing..\n");
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (a[i * n + j] != b[i * n + j]) {
-					printf("Error\n");
-					free(a); free(b);
-					return 0;
-				}
-			}
-		}
-		printf("Ok\n");
-	} else {
-		printf("Checking..\n");
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (a[i * n + j] != f(i, j)) {
-					printf("Error\n");
-					free(a); free(b);
-					return 0;
-				}
-			}
-		}
-		printf("Ok\n");
+	if (check(a, b, n, file)) {
+		free(a);
+		free(b);
+		return 0;
 	}
 
 	printf("Block %.2lf times faster than usual\n", t1 / t2);
@@ -122,6 +91,21 @@ printf("# %d\n", n);
 
 	printf("Cache-oblivious %.2lf times faster than usual\n", t1/t3);
 	
+	if (check(a, b, n, file)) {
+		free(a);
+		free(b);
+		return 0;
+	}
+
+	free(a);
+	free(b);
+	return 0;
+}
+
+int check(double * a, double * b, int n, const char * file) {
+	static int count = 1;
+	int res = 0;
+	count = (count == 1) ? 0 : 1;
 	if (file) {
 		printf("Checking now\nReading original file..\n");
 		if ((res = read_matrix(b, n, file)) < 0) {
@@ -135,17 +119,23 @@ printf("# %d\n", n);
 				default:
 					printf("Unknown error\n");
 			}
-			free(a);
-			free(b);
-			return -3;
+			return 1;
 		}
 		printf("Comparing..\n");
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				if (a[i * n + j] != b[j * n + i]) {
-					printf("Error\n");
-					free(a); free(b);
-					return 0;
+				if (count) {
+					if (a[i * n + j] != b[i * n + j]) {
+						printf("Error\n");
+						free(a); free(b);
+						return 0;
+					}
+				} else {
+					if (a[i * n + j] != b[j * n + i]) {
+						printf("Error\n");
+						free(a); free(b);
+						return 0;
+					}
 				}
 			}
 		}
@@ -154,18 +144,21 @@ printf("# %d\n", n);
 		printf("Checking..\n");
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				if (a[i * n + j] != f(j, i)) {
-					printf("Error\n");
-					free(a); free(b);
-					return 0;
+				if (count) {
+					if (a[i * n + j] != f(i, j)) {
+						printf("Error\n");
+						return 1;
+					}
+				} else {
+					if (a[i * n + j] != f(j, i)) {
+						printf("Error\n");
+						return 1;
+					}
 				}
 			}
 		}
 		printf("Ok\n");
 	}
-
-	free(a);
-	free(b);
 	return 0;
 }
 
